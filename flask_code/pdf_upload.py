b5b61b22ff96831f2fa7999d6a86df15215ca702
Andrew Davidson <acdavidson@sbcglobal.net>
2015-09-28 19:36:29 -0400

    changed uploader to be open to public IPs

--- flask_code/pdf_upload.py
+++ flask_code/pdf_upload.py
@@ -27,4 +27,5 @@ def uploaded_file(filename):
 	return send_from_directory(os.path.join(app.config['UPLOAD_FOLDER'], "pdfs"), filename)
 			
 if __name__=="__main__":
-	app.run(debug=True)
+	app.debug = true
+	app.run(host='0.0.0.0')
