b34be9936e8d8ace2157fd94d681144c6cecc26b
Tyler Duffy <duffytj@rose-hulman.edu>
2015-09-29 17:41:25 -0400

    added a sample unit test to test our <allowed_file> function inside of pdf_upload.py. I also grabbed a virtualenv in there.. not sure if that is something we want or not. I need it when using my local machine so maybe it helps. I doubt it. If needed, I can remove this.

---
+++ flask_code/TestPdfUpload.py
@@ -0,0 +1,29 @@
+import unittest
+import pdf_upload as pu
+
+class TestPdfUpload(unittest.TestCase):
+
+	# tests the 'allowed_file' function in the pdf_upload.py module
+  def test_allowed_file(self):
+   	print('-------------------------')
+  	print('-------------------------')
+  	print('--Testing Allowed File---')
+  	print('-------------------------')
+  	print('-------------------------')
+
+  	# list of allowable file extensions
+  	goodextensions = ['excel.pdf', 'word.pdf', 'powerpoint.pdf']
+  	for filext in goodextensions:
+  		print('Testing for file ext:', filext)
+  		# make sure these files are checked and are allowed
+  		self.assertTrue(pu.allowed_file(filext))
+
+  	# list of not allowed file extensions
+  	badextensions = ['excel.xlsx', 'word.docx', 'powerpoint.ppt, executable.exe']
+  	for filext in badextensions:
+  		print('Testing for file ext:', filext)
+  		# make sure these files are checked and NOT allowed
+  		self.assertFalse(pu.allowed_file(filext))
+
+if __name__ == '__main__':
+    unittest.main()
