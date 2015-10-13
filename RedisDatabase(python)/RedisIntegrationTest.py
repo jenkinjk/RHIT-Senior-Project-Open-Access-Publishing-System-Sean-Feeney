6fd9d058f440b0de91fe00c4beb85153628efc84
Andrew Davidson <davidsac@rose-hulman.edu>
2015-10-12 21:23:35 -0400

    fixed tabbing

--- RedisDatabase(python)/RedisIntegrationTest.py
+++ RedisDatabase(python)/RedisIntegrationTest.py
@@ -9,35 +9,29 @@ class RedisIntegrationTestCase(unittest.TestCase):
 
   def test_1(self):
     authorIDs = []
-	authorIDs.append(self.redisDB.putAuthor("Jimmy Fallon"))
-	authorIDs.append(self.redisDB.putAuthor("Jimmy Dean"))
-	authorIDs.append(self.redisDB.putAuthor("James Dean"))
-	authorIDs.append(self.redisDB.putAuthor("Dean Thomas"))
-	authorIDs.append(self.redisDB.putAuthor("Thomas Jefferson"))
-	authorIDs.append(self.redisDB.putAuthor("Jefferson Davis"))
-	
-	publishers = []
-	publishers.append(self.redisDB.putPublisher("RHIT"))
-	publishers.append(self.redisDB.putPublisher("McGraw-Hill"))
-	
-	tagIDs = []
-	tagIDs.append(self.redisDB.putTag("Biology"))
-	tagIDs.append(self.redisDB.putTag("Nanotechnology"))
-	tagIDs.append(self.redisDB.putTag("Distributed Computing"))
-	tagIDs.append(self.redisDB.putTag("Big Data"))
-	
-	paperIDs = []
-	paperIDs.append(self.redisDB.putPaper("MY TITLE IS IN CAPS", [authorIDs[0],authorIDs[5]], [tagIDs[0]], "This is an abstract", -1, datetime.datetime(2003, 8, 4, 12, 30, 45), publishers[0], [], []))
-	paperIDs.append(self.redisDB.putPaper("MY TITLE IS IN CAPS", [authorIDs[2],authorIDs[3]], [tagIDs[1],tagIDs[2]], "This is another abstract", -1, datetime.datetime(2004, 8, 4, 12, 30, 45), publishers[1], [], []))
-	
-	for paper in self.redisDB.getTopPapers():
-	  print paper
-	
-	
-  
-						 
+    authorIDs.append(self.redisDB.putAuthor("Jimmy Fallon"))
+    authorIDs.append(self.redisDB.putAuthor("Jimmy Dean"))
+    authorIDs.append(self.redisDB.putAuthor("James Dean"))
+    authorIDs.append(self.redisDB.putAuthor("Dean Thomas"))
+    authorIDs.append(self.redisDB.putAuthor("Thomas Jefferson"))
+    authorIDs.append(self.redisDB.putAuthor("Jefferson Davis"))
+    publishers = []
+    publishers.append(self.redisDB.putPublisher("RHIT"))
+    publishers.append(self.redisDB.putPublisher("McGraw-Hill"))
+    tagIDs = []
+    tagIDs.append(self.redisDB.putTag("Biology"))
+    tagIDs.append(self.redisDB.putTag("Nanotechnology"))
+    tagIDs.append(self.redisDB.putTag("Distributed Computing"))
+    tagIDs.append(self.redisDB.putTag("Big Data"))
+    paperIDs = []
+    paperIDs.append(self.redisDB.putPaper("MY TITLE IS IN CAPS", [authorIDs[0],authorIDs[5]], [tagIDs[0]], "This is an abstract", -1, datetime.datetime(2003, 8, 4, 12, 30, 45), publishers[0], [], []))
+    paperIDs.append(self.redisDB.putPaper("MY TITLE IS IN CAPS", [authorIDs[2],authorIDs[3]], [tagIDs[1],tagIDs[2]], "This is another abstract", -1, datetime.datetime(2004, 8, 4, 12, 30, 45), publishers[1], [], []))
+    for paper in self.redisDB.getTopPapers():
+      print paper
+    self.assertEqual('foo'.upper(), 'FOO')
+
   def tearDown(self):
     pass
-	
+
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
