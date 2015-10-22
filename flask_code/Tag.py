f29a42bb492eca2cfb8648e87988e82fe34c0573
Andrew Davidson <davidsac@rose-hulman.edu>
2015-10-22 13:18:54 -0400

    removed tagID

--- flask_code/Tag.py
+++ flask_code/Tag.py
@@ -6,14 +6,13 @@ Created on Oct 1, 2015
 
 class Tag:
 
-  def __init__(self, id, name, viewCount, papers):
-    self.id = id
+  def __init__(self, name, viewCount, papers):
     self.name = name
     self.viewCount = viewCount
     self.papers = papers
 
   def __str__(self):
-    return 'id:'+self.id+'    name:'+self.name+'   papers:'+str(self.papers)+'      viewCount:'+self.viewCount
+    return 'name:'+self.name+'   papers:'+str(self.papers)+'      viewCount:'+self.viewCount
 
   def __repr__(self):
-    return 'id:'+self.id+'    name:'+self.name+'   papers:'+str(self.papers)+'      viewCount:'+self.viewCount      
+    return 'name:'+self.name+'   papers:'+str(self.papers)+'      viewCount:'+self.viewCount      
