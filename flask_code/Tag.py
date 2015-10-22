80d0737bdca3ac8319e0b370771be4c879d4fb4d
Andrew Davidson <davidsac@rose-hulman.edu>
2015-10-22 13:23:37 -0400

    restored old version of tag

--- flask_code/Tag.py
+++ flask_code/Tag.py
@@ -6,13 +6,14 @@ Created on Oct 1, 2015
 
 class Tag:
 
-  def __init__(self, name, viewCount, papers):
+  def __init__(self, id, name, viewCount, papers):
+    self.id = id
     self.name = name
     self.viewCount = viewCount
     self.papers = papers
 
   def __str__(self):
-    return 'name:'+self.name+'   papers:'+str(self.papers)+'      viewCount:'+self.viewCount
+    return 'id:'+self.id+'    name:'+self.name+'   papers:'+str(self.papers)+'      viewCount:'+self.viewCount
 
   def __repr__(self):
-    return 'name:'+self.name+'   papers:'+str(self.papers)+'      viewCount:'+self.viewCount      
+    return 'id:'+self.id+'    name:'+self.name+'   papers:'+str(self.papers)+'      viewCount:'+self.viewCount      
