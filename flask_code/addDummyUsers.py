dbb3c363976822a34232525f690add416dec364f
Tyler Duffy <duffytj@rose-hulman.edu>
2015-11-03 00:08:59 -0500

    updated addDummyUsers

--- flask_code/addDummyUsers.py
+++ flask_code/addDummyUsers.py
@@ -2,8 +2,9 @@
 import User
 from RedisDatabase import RedisDatabase
 
-db = RedisDatabase("Anything besides the string 'Test', which wipes the database each time for testing purposes") # our wrapper for the database
+#db = RedisDatabase("Anything besides the string 'Test', which wipes the database each time for testing purposes") # our wrapper for the database
+db = RedisDatabase("Test") # our wrapper for the database
 
 db.putUser("Asher Morgan", "1162476383780112")
 db.putUser("Jonathan Jenkins", "986584014732857")
-
+db.putUser("Tyler Duffy", "10153554827465751")
